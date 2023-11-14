e901fdc85067f60c907fac2581d47ed4cc8164e7
NeonNatural <fawazalsafadi@gmail.com>
2023-11-14 14:57:15 +0000

    Added Backup Storage private module

--- main.tf
+++ main.tf
@@ -204,8 +204,8 @@ resource "null_resource" "configure-cat-app" {
 }
 
 
-module "azure-backup" {
-  source              = "app.terraform.io/Terraform-workshopFawaz/azure-backup/azurerm"
-  version             = "1.0.2"
-  resource_group_name = "${var.prefix}-public"
-}
+# module "azure-backup" {
+#   source              = "app.terraform.io/Terraform-workshopFawaz/azure-backup/azurerm"
+#   version             = "1.0.2"
+#   resource_group_name = "${var.prefix}-public"
+# }
